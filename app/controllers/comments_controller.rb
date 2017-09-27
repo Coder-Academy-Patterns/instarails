73a91de6d83806b998e0dcb309ee6d70e0b177d4
Patrick Smith <patrick@burntcaramel.com>
2017-09-27 14:31:15 +1000

    Improve comment appearance

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,7 +42,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to photo_comments_url(@photo), notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to photo_comments_url(@photo), notice: 'Comment was successfully deleted.' }
       format.json { head :no_content }
     end
   end
