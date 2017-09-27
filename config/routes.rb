dcdd99f7d359e10188960cb2a6ebc8ce50f0c682
Patrick Smith <patrick@burntcaramel.com>
2017-09-27 13:06:18 +1000

    Add commenting on photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :photos
   root 'landing#index'
+  devise_for :users
+  resources :photos do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927003457) do
+ActiveRecord::Schema.define(version: 20170927015855) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "photo_id"
